0cf16143f25da497a6fddef631153383892efdc7
VenkatesaMG <m.g.venkatesa@gmail.com>
2025-01-13 21:45:10 +0530

    User's lab allocation body structure is framed

--- lab_alloc/src/App.jsx
+++ lab_alloc/src/App.jsx
@@ -3,6 +3,7 @@ import { CiCircleQuestion } from "react-icons/ci";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { SiGooglegemini } from "react-icons/si";
 import Calendar from "./components/calender.jsx";
+import CustomSelect from "./components/custom_select.jsx";
 function App() {
   return (
     <>
@@ -39,7 +40,38 @@ function App() {
         </div>
       </nav>
       <div className="canvas">
-        
+        <div className="canvas-inner-div">
+          <div>
+            <h2 style={{ fontFamily: "Roboto", padding: "0px 6px" }}>
+              Lab Utilization
+            </h2>
+          </div>
+          <nav className="lab-nav">
+            <button>Overview</button>
+            <button>Calendar</button>
+            <button>List</button>
+            <button>Heatmap</button>
+          </nav>
+          <div className="canvas-comp">
+            <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+              <div style={{ padding: "0px 6px", fontWeight: "600" }}>
+                Calendar
+              </div>
+              <div>
+                <CustomSelect />
+              </div>
+            </div>
+            <div style={{ margin: "0rem 1rem" }}>
+              <Calendar />
+            </div>
+            <div style={{ padding: "0px 6px", fontWeight: "600" }}>
+              <div>Recent Bookings</div>
+              <div className="recent-book-div">
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ lab_alloc/src/components/custom_select.jsx
@@ -0,0 +1,14 @@
+export default function CustomSelect() {
+  return (
+    <div>
+      <select name="labs" class="lab-select" aria-placeholder="Lab">
+        <option value="dog">Dog</option>
+        <option value="cat">Cat</option>
+        <option value="hamster">Hamster</option>
+        <option value="parrot">Parrot</option>
+        <option value="spider">Spider</option>
+        <option value="goldfish">Goldfish</option>
+      </select>
+    </div>
+  );
+}
