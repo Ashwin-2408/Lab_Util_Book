54f03c28b1f192af887d4ce7c087429fde7996c8
VenkatesaMG <m.g.venkatesa@gmail.com>
2025-01-18 17:09:59 +0530

    Designed Card for the Events to be displayed in the time line

--- lab_alloc/src/components/card.jsx
+++ lab_alloc/src/components/card.jsx
@@ -1,3 +1,15 @@
+import { DomainVerification } from "@mui/icons-material";
+import { DiAtom } from "react-icons/di";
 export default function Card() {
-  return <div></div>;
+  return( 
+      <div className="event-card-outer">
+          <div className="event-card">
+          <div><DiAtom style={{fontSize:'1.5rem'}}/></div>
+          <div className="event-card-body">
+            <div style={{fontWeight:'500'}}>Kevin</div>
+            <div style={{fontSize:'0.7rem'}}>9:00 - 10:00</div>
+          </div>
+        </div>
+      </div>
+);
 }
--- lab_alloc/src/main.jsx
+++ lab_alloc/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainApp from "./main_app.jsx";
-import NewSession from "./components/new_session.jsx";
+import Card from "./components/card.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainApp />
+    <Card />
   </StrictMode>
 );
