8b8a543a79962cbc523c72313f548aab2a21914e
AShwin <ashwin.thanga@gmail.com>
2025-02-22 09:12:27 +0530

    Lab and resource schema completed

---
+++ lab_alloc/Node_Backend/Schema/Lab.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Lab = sequelize.define(
+  "Lab",
+  {
+    lab_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    lab_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    total_systems: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    available_systems: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default Lab;
