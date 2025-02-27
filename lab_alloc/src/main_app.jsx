755cdcef0da4a2ecb3ac616cf9c1f3b16c532be2
VenkatesaMG <m.g.venkatesa@gmail.com>
2025-02-28 02:17:54 +0530

    Added Approval Module for Lab Consents

--- lab_alloc/src/main_app.jsx
+++ lab_alloc/src/main_app.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import NewSession from "./components/new_session.jsx";
-import ApproveSession from "./components/approve_sess.jsx";
 import Scanner from "./components/scanner.jsx";
-import CheckIn from "./components/check_in_div.jsx";
 import CancelLab from "./components/CancelLab.jsx";
+import Approve from "./components/approve_sess.jsx";
+import ApprovePopUp from "./components/approve_popup.jsx";
 function MainApp() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/book" element={<NewSession />} />
-        <Route path="/temp" element={<Scanner />} />
+        <Route path="/scan" element={<Scanner />} />
         <Route path="/cancel-lab" element={<CancelLab />} />
-
+        <Route path="/temp" element={<Approve />} />
       </Routes>
     </Router>
   );
